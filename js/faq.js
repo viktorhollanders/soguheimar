0ef3ad9ac639b21c99ad70c6b175d05acc163bb3
Viktor Hollanders <viktorhollanders@gmail.com>
2019-06-11 21:51:52 +0000

    finis FAQ for sumarbudir

--- js/faq.js
+++ js/faq.js
@@ -1,26 +1,9 @@
-//when I click the plus
-//rotat the plus sign and add class active to the current clicked item
-const openCloseImg = document.querySelectorAll("img.openCloseImage");
-const answerContentTag = document.querySelectorAll("p.QandA-answer__content");
-const openCloseTag = document.querySelectorAll("img.openClose");
+const listItem = document.querySelectorAll("li.faq-list__item");
 
-const showAnswer = function() {
-  openCloseImg.forEach(img => {
-    img.style.transform = "rotate(45deg)";
-  });
+listItem.forEach(listItem => {
+  const opener = listItem.querySelector("img.opener");
 
-  answerContentTag.forEach(tag => {
-    tag.style.display = "block";
-  });
-};
-
-// when I click a button toggle showAnswer function
-
-openCloseTag.forEach(tag => {
-  tag.addEventListener("click", function() {
-    showAnswer();
+  opener.addEventListener("click", function() {
+    listItem.classList.toggle("open");
   });
 });
-
-
-//maybe add add/remove class to toggle behavior
