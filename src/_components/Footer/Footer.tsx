9a1e952e4e91777ccb7b71c12555b6ca7e8fa1aa
viktorhollanders <viktorhollanders@hey.com>
2025-05-03 13:40:27 +0000

    Compleet footer markup and styling

--- src/_components/Footer/Footer.tsx
+++ src/_components/Footer/Footer.tsx
@@ -36,12 +36,35 @@ export default function Footer() {
                 <p className={styles.address}>Lækjarbotnalandi 53</p>
                 <p className={styles.address}>203 Kópavogur</p>
                 <p className={styles.address}>Ísland</p>
-                <Link className={styles.mapLink} href={mapLink}>
+                <Link
+                    id={styles.mapLink}
+                    className={styles.link}
+                    href={mapLink}
+                >
                     Sjá kort
                 </Link>
             </div>
             <div className={styles.infoContainer}>
                 <h2 className={styles.infoHeader}>Hafa samband</h2>
+                <div className={styles.contactItem}>
+                    <p className={styles.contactLable}>Email:</p>
+                    <a
+                        className={styles.link}
+                        href="mailto:söguheimar@gmail.com"
+                    >
+                        söguheimar@gmail.com
+                    </a>
+                </div>
+                <div className={styles.contactItem}>
+                    <p className={styles.contactLable}>Phone:</p>
+                    <a className={styles.link} href="tel:8993377">
+                        899-3377
+                    </a>
+                    <span className={styles.seperator}>/</span>
+                    <a className={styles.link} href="tel:8921142">
+                        892-1142
+                    </a>
+                </div>
             </div>
             <p className={styles.copyright}>© 2025 Söguheimar</p>
         </footer>
