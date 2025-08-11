2cf228a05412968279f0eb9377cfdbe43a29ac73
viktorhollanders <viktorhollanders@hey.com>
2025-08-11 21:13:17 +0000

    Update card image, fix cource card image position, remove line hight from global css

--- src/_components/Card/Card.tsx
+++ src/_components/Card/Card.tsx
@@ -1,47 +1,47 @@
-import Image from "next/image";
-import Button from "../Button/Button";
-import styles from "./styles.module.css";
+import Image from 'next/image';
+import Button from '../Button/Button';
+import styles from './styles.module.css';
 
 type CardProps = {
-    iconUrl: string;
-    alt_description: string;
-    price: number;
-    weekNr: number;
-    dateFrom: string;
-    dateTo: string;
-    link: string;
+  iconUrl: string;
+  alt_description: string;
+  price: number;
+  weekNr: number;
+  dateFrom: string;
+  dateTo: string;
+  link: string;
 };
 
 export default function Card({
-    iconUrl,
-    alt_description,
-    price,
-    weekNr,
-    dateFrom,
-    dateTo,
-    link,
+  iconUrl,
+  alt_description,
+  price,
+  weekNr,
+  dateFrom,
+  dateTo,
+  link,
 }: CardProps) {
-    const formatedPrice = price.toLocaleString("de-DE");
+  const formatedPrice = price.toLocaleString('de-DE');
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.cardHeader}>
-                <Image
-                    src={iconUrl}
-                    alt={alt_description}
-                    className={styles.icon}
-                    width={40}
-                    height={40}
-                />
-                <div className={styles.info}>
-                    <p className={styles.weekInfo}>Week {`${weekNr}`}</p>
-                    <p className={styles.date_info}>
-                        {`${dateFrom}`} — {`${dateTo}`}
-                    </p>
-                </div>
-            </div>
-            <p className={styles.cardPrice}>{formatedPrice} kr</p>
-            <Button text="Ská" url={link} />
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <Image
+          className={styles.cardImage}
+          src={iconUrl}
+          alt={alt_description}
+          width={56}
+          height={56}
+        />
+        <div className={styles.info}>
+          <p className={styles.weekInfo}>Vika {`${weekNr}`}</p>
+          <p className={styles.date_info}>
+            {`${dateFrom}`} — {`${dateTo}`}
+          </p>
         </div>
-    );
+      </div>
+      <p className={styles.cardPrice}>{formatedPrice} kr</p>
+      <Button text="Ská" url={link} />
+    </div>
+  );
 }
--- src/_components/Courses/Cources.tsx
+++ src/_components/Courses/Cources.tsx
@@ -1,45 +1,54 @@
-import styles from "./styles.module.css";
-import Card from "../Card/Card";
+import styles from './styles.module.css';
+import Card from '../Card/Card';
 
 export default function Courses() {
-    const cources = [
-        {
-            iconUrl: "/images/oz-logo-2.png",
-            weekNr: 1,
-            dateFrom: "23. june",
-            dateTo: "28. june",
-            price: 60000,
-            link: "/",
-            iconAlt: "A white dragon flying over a forest",
-        },
-        {
-            iconUrl: "/images/oz-logo-2.png",
-            weekNr: 2,
-            dateFrom: "1. july",
-            dateTo: "6. july",
-            price: 60000,
-            link: "/",
-            iconAlt: "A white dragon flying over a forest",
-        },
-    ];
+  const cources = [
+    {
+      iconUrl: '/images/nes_icon.jpg',
+      weekNr: 1,
+      dateFrom: '23. june',
+      dateTo: '28. june',
+      price: 60000,
+      link: '/',
+      iconAlt: 'A white dragon flying over a forest',
+    },
+    {
+      iconUrl: '/images/nes_icon.jpg',
+      weekNr: 2,
+      dateFrom: '1. july',
+      dateTo: '6. july',
+      price: 60000,
+      link: '/',
+      iconAlt: 'A white dragon flying over a forest',
+    },
+    {
+      iconUrl: '/images/nes_icon.jpg',
+      weekNr: 3,
+      dateFrom: '10. july',
+      dateTo: '16. july',
+      price: 60000,
+      link: '/',
+      iconAlt: 'A white dragon flying over a forest',
+    },
+  ];
 
-    return (
-        <section className={styles.courseContainer}>
-            <h1 className={styles.mainHeading}>Námskeið</h1>
-            <h2 className={styles.subHeading}>Sagan endalausa</h2>
+  return (
+    <section className={styles.courseContainer}>
+      <h1 className={styles.mainHeading}>Námskeið</h1>
+      <h2 className={styles.subHeading}>Sagan endalausa</h2>
 
-            {cources.map((cources, index) => (
-                <Card
-                    key={index}
-                    iconUrl={cources.iconUrl}
-                    weekNr={cources.weekNr}
-                    dateFrom={cources.dateFrom}
-                    dateTo={cources.dateTo}
-                    price={cources.price}
-                    link={cources.link}
-                    alt_description={cources.iconAlt}
-                />
-            ))}
-        </section>
-    );
+      {cources.map((cources, index) => (
+        <Card
+          key={index}
+          iconUrl={cources.iconUrl}
+          weekNr={cources.weekNr}
+          dateFrom={cources.dateFrom}
+          dateTo={cources.dateTo}
+          price={cources.price}
+          link={cources.link}
+          alt_description={cources.iconAlt}
+        />
+      ))}
+    </section>
+  );
 }
